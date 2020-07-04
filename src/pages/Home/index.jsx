479dcacd097f075fb4d0751dcf7fa19a41fc9603
Murphyly <isysnogueira@infojr.com.br>
2020-07-04 16:24:16 -0300

    init first page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom'
 import { Container, Button } from './style'
 
 import Header from '../../components/Header'
-import logo from '../../assets/logo_test.jpeg'
+import logo from '../../assets/logo.png'
 
 const Home = () => {
   return (
     <>
       <Header />
 
-      <Container>
+      <Container className="bg-light">
         <div>
           <img src={logo} alt="logo" width={'100%'} height={'50%'} />
-          <Link to="/map-navigation">
-            <Button> Procurar </Button>
+          <Link className="d-flex justify-content-center pt-5" to="/map-navigation">
+            <Button className="btn btn-outline-danger btn-lg"> Procurar </Button>
           </Link>
         </div>
       </Container>
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -3,11 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
   align-items: center;
   display: flex;
-  height: 90%;
+  min-height: 638px;
 `
 export const Button = styled.button`
-  border-radius: 5px;
-  width: 200px;
-  height: 50px;
-  background-color: rgba(0, 173, 0, 0.3);
 `
